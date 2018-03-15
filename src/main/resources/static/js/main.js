269abb1319a46504f190fd833bf45403381460d7
mangrove108 <j.geffken@icloud.com>
2018-03-15 12:54:27 +0100

    delete item, new item

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,7 +3,41 @@
 */
 
 $(document).ready(function(){
-	$('.table .eBtn').on('click', function(event){
-		$('.myForm #exampleModal').modal();
+	
+	$('.nBtn, .table .eBtn').on('click', function(event){
+		
+		event.preventDefault();
+		var href = $(this).attr('href');
+		var text = $(this).text();
+		
+		/* Edit Items */
+		if(text == 'Edit'){
+			
+			$.get(href, function(country, status){
+				$('.myForm #id').val(country.id);
+				$('.myForm #name').val(country.name);
+				$('.myForm #capital').val(country.capital);
+			});
+			
+			$('.myForm #exampleModal').modal();
+			
+		} else {
+			
+			$('.myForm #id').val('');
+			$('.myForm #name').val('');
+			$('.myForm #capital').val('');
+			
+			$('.myForm #exampleModal').modal();
+		}	
+	});
+	
+	/* Delete Items */
+	$('.table .delBtn').on('click', function(event){
+		event.preventDefault();
+		var href = $(this).attr('href');
+		$('#myModal2 #delRef').attr('href', href);
+		
+		$('.myModal2 #myModal2').modal();
+		
 	});
 });
